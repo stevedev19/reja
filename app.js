12ba1f2b1f0df3e667de21e5cdfad5b3e36f7c1d
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-06-08 15:09:48 +0900

    feat: mongoDB ulanish logikasi

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log("Web Serverni boshlash");
+const express = require("express"); 
+
+const app = express(); 
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
+
+/*console.log("I am just checking whether it is working or not");
+const express = require("express");
+const app = express();
+const http = require("http"); */
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+
+// 1: Kirish codes
+app.use(express.static("public")); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+// 2: Session codes - bugun kerak emas
+
+// 3: View codes
+app.set("views", "views"); 
+app.set("view engine", "ejs"); 
+
+// 4: Rooting codes
+app.post("/create-item", (req, res) => {
+    /*console.log(req.body);
+    res.json({test: "success"}); from old reja*/
+ // TODO: code with db here
+})
+clearInterval
+app.get("/author", (req, res) =>{
+    res.render("author", { user: user });
+});
+
+app.get("/", function(req, res){
+    res.render("reja");
+});
+
+/*app.get("/", function (req, res) {
+  res.end("<h1>HELLO WORLD</h1>");
+});
+
+app.get("/gift", function (req, res) {
+  res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
+}); from old reja express */
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,61 +1,29 @@
-console.log("Web Serverni boshlash");
-const express = require("express"); 
 const http = require("http"); 
-const app = express(); 
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
+const mongodb = require("mongodb"); 
 
-/*console.log("I am just checking whether it is working or not");
-const express = require("express");
-const app = express();
-const http = require("http"); */
+let db;
+const connectionString = 
+'mongodb+srv://abdullatoshtemirov:9lgTJjaXxjk7Bs7q@cluster0.ruwrd2y.mongodb.net/Reja'
 
-// 1: Kirish codes
-app.use(express.static("public")); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUNifiedTopology: true,
 
-// 2: Session codes - bugun kerak emas
-
-// 3: View codes
-app.set("views", "views"); 
-app.set("view engine", "ejs"); 
-
-// 4: Rooting codes
-app.post("/create-item", (req, res) => {
-    /*console.log(req.body);
-    res.json({test: "success"}); from old reja*/
- // TODO: code with db here
-})
-clearInterval
-app.get("/author", (req, res) =>{
-    res.render("author", { user: user });
-});
-
-app.get("/", function(req, res){
-    res.render("reja");
-});
-
-/*app.get("/", function (req, res) {
-  res.end("<h1>HELLO WORLD</h1>");
-});
-
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
-}); from old reja express */
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
+}, (err, client) => {
+   if(err) console.log("ERROR on connection Mongo DB");
+   else{
+    console.log("MongoDB connection succeed");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function () {
   console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
   );
 });
+   }
+}
+);
+
+
